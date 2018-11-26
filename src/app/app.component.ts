7b3bce3f437ecdcee4b2a13d6e5b8576dc9bc550
alka653 <alka65@hotmail.com>
2018-11-26 15:09:27 -0500

    last updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import executeSQL from './sql-server';
+import * as $ from 'jquery';
+import executeSQL from './executeSQL.js';
 
 @Component({
 	selector: 'app-root',
@@ -9,33 +10,34 @@ import executeSQL from './sql-server';
 export class AppComponent {
 	cliente:any = {};
 	title = 'ChallengeMadecentro';
-	onSubmit(){
-		const clienteData = this.cliente
-		executeSQL(`insert into clientes(
-			numero_cedula,
-			nombre,
-			apellido,
-			direccion_residencia,
-			numero_telefonico,
-			numero_celular,
-			email,
-			puntos
-		) values(
-			'${clienteData.numero_cedula}'
-			'${clienteData.nombre}',
-			'${clienteData.apellido}',
-			'${clienteData.direccion_residencia}',
-			'${clienteData.numero_telefono}',
-			'${clienteData.numero_celular}',
-			'${clienteData.email}',
-			0
-		)`)
-		executeSQL(`select count(cliente_cedula) from tmp_ventas_cliente where cliente_cedula = '${clienteData.numero_cedula}'`, false).on('row', function(columns){
-			if(Object.keys(columns).length > 0){
-				executeSQL(`update clientes set puntos = ${Object.keys(columns).length * 1000} where numero_cedula = ${clienteData.numero_cedula}`)
-			}
+	ngOnInit(): void {
+		$('form').submit(function(event){
+			event.preventDefault()
+			executeSQL(`insert into clientes(
+				numero_cedula,
+				nombre,
+				apellido,
+				direccion_residencia,
+				numero_telefonico,
+				numero_celular,
+				email,
+				puntos
+			) values(
+				'0'
+				'0,
+				'0,
+				'0,
+				'0,
+				'0,
+				'0,
+				0
+			)`)
+			/*executeSQL(`select count(cliente_cedula) from tmp_ventas_cliente where cliente_cedula = '${clienteData.numero_cedula}'`, false).on('row', function(columns){
+				if(Object.keys(columns).length > 0){
+					executeSQL(`update clientes set puntos = ${Object.keys(columns).length * 1000} where numero_cedula = ${clienteData.numero_cedula}`)
+				}
+			})*/
+			alert('Cliente guardado con éxito!')
 		})
-		alert('Cliente guardado con éxito!')
-		this.cliente = {}
 	}
 }
